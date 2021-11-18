5d8f960d76957f83ddbef6cf5c4ee2291b7bb2e7
unknown <lavinia-ozana.onofrei@student.tuiasi.ro>
2021-11-18 13:30:10 +0200

    S7-interface

---
+++ interface.py
@@ -0,0 +1,72 @@
+from tkinter import *
+
+
+class App(Frame):
+    def __init__(self, app):
+        Frame.__init__(self, app)
+        app.title("App RC")
+        app.resizable(0, 0)
+        self.labelIpSender = Label(app, text="IP Sender:")
+        self.entryIpSender = Entry(app)
+        self.labelIpSender.grid(row=1, column=0, sticky='NW')
+        self.entryIpSender.grid(row=1, column=1, sticky='NW')
+
+        self.labelPortSender = Label(app, text="Port:")
+        self.entryPortSender = Entry(app)
+        self.labelPortSender.grid(row=2, column=0, sticky='NW')
+        self.entryPortSender.grid(row=2, column=1, sticky='NW')
+
+        self.labelIpReceiver = Label(app, text="IP Receiver:")
+        self.entryIpReceiver = Entry(app)
+        self.labelIpReceiver.grid(row=3, column=0, sticky='NW')
+        self.entryIpReceiver.grid(row=3, column=1, sticky='NW')
+
+        self.labelPortReceiver = Label(app, text="Port:")
+        self.entryPortReceiver = Entry(app)
+        self.labelPortReceiver.grid(row=4, column=0, sticky='NW')
+        self.entryPortReceiver.grid(row=4, column=1, sticky='NW')
+
+        self.labelTimeout = Label(app, text="Timeout(ms):")
+        self.entryTimeout = Entry(app)
+        self.labelTimeout.grid(row=5, column=0, sticky='NW')
+        self.entryTimeout.grid(row=5, column=1, sticky='NW')
+        self.entryTimeout.insert('end', "5000")
+
+        self.labelWinSize = Label(app, text="Dimensiunea ferestrei glisante:")
+        self.labelWinSize.grid(row=6, column=0, rowspan=6, sticky='NW')
+        self.scaleWinSize = Scale(app, orient=HORIZONTAL, length=250, from_=1, to=100, resolution=1)
+        self.scaleWinSize.grid(row=7, column=0, columnspan=2, sticky='N')
+
+        self.labelFailure = Label(app, text="Sansa de a pierde un pachet:")
+        self.entryFailure = Entry(app)
+        self.labelFailure.grid(row=11, column=0, rowspan=6, sticky='NW')
+        self.entryFailure.grid(row=11, column=1, sticky='NW')
+        self.entryFailure.insert('end', "0.1")
+
+        self.buttonConfiguration = Button(text='Configurare')
+        self.buttonConfiguration.grid(row=12, column=0, columnspan=2, pady=10, sticky='N')
+
+        self.buttonOpenFile = Button(text='Deschidere fisier')
+        self.buttonOpenFile.grid(row=23, column=0, columnspan=2, sticky='N')
+
+        self.labelSenderView = Label(app, text="View Sender")
+        self.entrySenderView = Text(app, state=DISABLED, height=15, wrap=WORD)
+        self.scrollbarSenderView = Scrollbar(app, command=self.entrySenderView.yview)
+        self.entrySenderView.config(yscrollcommand=self.scrollbarSenderView.set)
+        self.labelSenderView.grid(row=0, column=2, columnspan=2, sticky='N')
+        self.entrySenderView.grid(row=1, column=2, rowspan=6, columnspan=2, padx=10, sticky='N')
+        self.scrollbarSenderView.grid(row=1, column=3, ipady=97, rowspan=6, sticky='NE')
+
+        self.labelReceiverView = Label(app, text="View Receiver")
+        self.entryReceiverView = Text(app, state=DISABLED, height=15, wrap=WORD)
+        self.scrollbarReceiverView = Scrollbar(app, command=self.entryReceiverView.yview)
+        self.entryReceiverView.config(yscrollcommand=self.scrollbarReceiverView.set)
+        self.labelReceiverView.grid(row=8, column=2, columnspan=2, sticky='N')
+        self.entryReceiverView.grid(row=9, column=2, rowspan=15, columnspan=2, padx=10, sticky='N')
+        self.scrollbarReceiverView.grid(row=9, column=3, ipady=97, rowspan=15, sticky='NE')
+
+        self.buttonStart = Button(app, text="START")
+        self.buttonStart.grid(row=25, column=2, pady=10)
+
+        self.buttonStart = Button(app, text="STOP")
+        self.buttonStart.grid(row=25, column=3, pady=10)
---
+++ main.py
@@ -0,0 +1,8 @@
+from interface import *
+from tkinter import *
+
+
+if __name__ == "__main__":
+    root = Tk()
+    app = App(root)
+    app.mainloop()
