5951c122b94826f9f876689ea5fef1d080cdd180
unknown <lavinia-ozana.onofrei@student.tuiasi.ro>
2021-11-19 09:15:03 +0200

    S7-modificari2

--- interface.py
+++ interface.py
@@ -4,30 +4,6 @@ from tkinter import *
 class App(Frame):
     def __init__(self, app, **kw):
         super().__init__(**kw)
-        self.buttonStart = Button(app, text="STOP")
-        self.scrollbarReceiverView = Scrollbar(app, command=self.entryReceiverView.yview)
-        self.entryReceiverView = Text(app, state=DISABLED, height=15, wrap=WORD)
-        self.labelReceiverView = Label(app, text="View Receiver")
-        self.scrollbarSenderView = Scrollbar(app, command=self.entrySenderView.yview)
-        self.entrySenderView = Text(app, state=DISABLED, height=15, wrap=WORD)
-        self.labelSenderView = Label(app, text="View Sender")
-        self.buttonOpenFile = Button(text='Deschidere fișier')
-        self.buttonConfiguration = Button(text='Configurare')
-        self.entryFailure = Entry(app)
-        self.labelFailure = Label(app, text="Șansa de a pierde un pachet:")
-        self.entryIpSender = Entry(app)
-        self.labelIpSender = Label(app, text="IP Sender:")
-        self.labelPortSender = Label(app, text="Port:")
-        self.entryPortSender = Entry(app)
-        self.labelIpReceiver = Label(app, text="IP Receiver:")
-        self.entryIpReceiver = Entry(app)
-        self.labelPortReceiver = Label(app, text="Port:")
-        self.entryPortReceiver = Entry(app)
-        self.labelTimeout = Label(app, text="Timeout(ms):")
-        self.entryTimeout = Entry(app)
-        self.labelWinSize = Label(app, text="Dimensiunea ferestrei glisante:")
-        self.scaleWinSize = Scale(app, orient=HORIZONTAL, length=250, from_=1, to=100, resolution=1)
-
         self.IpSender = ''
         self.PortSender = 0
         self.IpReceiver = ''
@@ -50,6 +26,8 @@ class App(Frame):
         Frame.__init__(self, app)
         app.title("Transfer de fișiere")
         app.resizable(0, 0)
+        self.labelIpSender = Label(app, text="IP Sender:")
+        self.entryIpSender = Entry(app)
         self.labelIpSender.grid(row=1, column=0, sticky='NW')
         self.entryIpSender.grid(row=1, column=1, sticky='NW')
 
@@ -79,28 +57,46 @@ class App(Frame):
         self.scaleWinSize = Scale(app, orient=HORIZONTAL, length=250, from_=1, to=100, resolution=1)
         self.scaleWinSize.grid(row=7, column=0, columnspan=2, sticky='N')
 
+        self.labelPackSize = Label(app, text="Dimensiunea pachetului:")
+        self.labelPackSize.grid(row=9, column=0, rowspan=6, sticky='NW')
+        self.entryPackSize=Entry(app)
+        self.entryPackSize.grid(row=9, column=1, sticky='NE')
+
+        self.labelFailure = Label(app, text="Șansa de a pierde un pachet:")
+        self.entryFailure = Entry(app)
         self.labelFailure.grid(row=11, column=0, rowspan=6, sticky='NW')
         self.entryFailure.grid(row=11, column=1, sticky='NW')
         self.entryFailure.insert('end', "0.1")
 
+
+
+        self.buttonConfiguration = Button(text='Configurare')
         self.buttonConfiguration.grid(row=12, column=0, columnspan=2, pady=10, sticky='N')
 
+        self.buttonOpenFile = Button(text='Deschidere fișier')
         self.buttonOpenFile.grid(row=23, column=0, columnspan=2, sticky='N')
 
+        self.labelSenderView = Label(app, text="View Sender")
+        self.entrySenderView = Text(app, state=DISABLED, height=15, wrap=WORD)
+        self.scrollbarSenderView = Scrollbar(app, command=self.entrySenderView.yview)
         self.entrySenderView.config(yscrollcommand=self.scrollbarSenderView.set)
         self.labelSenderView.grid(row=0, column=2, columnspan=2, sticky='N')
         self.entrySenderView.grid(row=1, column=2, rowspan=6, columnspan=2, padx=10, sticky='N')
         self.scrollbarSenderView.grid(row=1, column=3, ipady=97, rowspan=6, sticky='NE')
 
+        self.labelReceiverView = Label(app, text="View Receiver")
+        self.entryReceiverView = Text(app, state=DISABLED, height=15, wrap=WORD)
+        self.scrollbarReceiverView = Scrollbar(app, command=self.entryReceiverView.yview)
         self.entryReceiverView.config(yscrollcommand=self.scrollbarReceiverView.set)
         self.labelReceiverView.grid(row=8, column=2, columnspan=2, sticky='N')
         self.entryReceiverView.grid(row=9, column=2, rowspan=15, columnspan=2, padx=10, sticky='N')
         self.scrollbarReceiverView.grid(row=9, column=3, ipady=97, rowspan=15, sticky='NE')
 
-        self.buttonStart = Button(app, text="START")
+        self.buttonStart = Button(app, text="START", bg='green')
         self.buttonStart.grid(row=25, column=2, pady=10)
 
-        self.buttonStart.grid(row=25, column=3, pady=10)
+        self.buttonStop = Button(app, text="STOP", bg='red')
+        self.buttonStop.grid(row=25, column=3, pady=10)
 
     def validateInput(self):
         config = False
